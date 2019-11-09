be5a945b319a696dc69506397be335586aa821d7
arashcodes <s.arash.abbasi@gmail.com>
2019-11-08 17:19:07 -0800

    Adds random command from server

--- server/js/httpHandler.js
+++ server/js/httpHandler.js
@@ -15,6 +15,12 @@ module.exports.initialize = (queue) => {
 module.exports.router = (req, res, next = ()=>{}) => {
   console.log('Serving request type ' + req.method + ' for url ' + req.url);
   res.writeHead(200, headers);
-  res.end();
+
+ let commands = ['up', 'down', 'right', 'left'];
+  let randomCommand = commands[Math.floor(Math.random() * commands.length)];
+
+
+
+  res.end(randomCommand);
   next(); // invoke next() at the end of a request to help with testing!
 };
